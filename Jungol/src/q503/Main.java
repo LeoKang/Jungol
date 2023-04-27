2518d2c5a0021eb0ed5b35d5d23fc7e0082160fc
Class01 <Class01@DESKTOP-9H6B9C5>
2023-04-27 09:53:00 +0900

    503

---
+++ Jungol/src/q503/Main.java
@@ -0,0 +1,12 @@
+package q503;
+
+public class Main {
+	public static void main(String[] args) {
+		//1st case
+//		System.out.println("My name is Hong Gil Dong.");
+//		System.out.println("I am 13 years old.");
+		
+		//2nd case
+		System.out.println("My name is Hong Gil Dong.\nI am 13 years old.");
+	}
+}
