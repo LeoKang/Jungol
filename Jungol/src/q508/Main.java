7ed02460191e7892836eac9fc4938122cfb0d7a3
Class01 <Class01@DESKTOP-9H6B9C5>
2023-04-27 10:21:03 +0900

    508번 2nd case

---
+++ Jungol/src/q508/Main.java
@@ -0,0 +1,17 @@
+package q508;
+
+public class Main {
+	public static void main(String[] args) {
+		// 1st case
+//		System.out.println("      item     count     price");
+//		System.out.println("       pen        20       100");
+//		System.out.println("      note         5        95");
+//		System.out.println("    eraser       110        97");
+
+		// 2nd case
+		System.out.printf("%10s%10s%10s\n", "item", "count", "price");
+		System.out.printf("%10s%10s%10s\n", "pen", "20", "100");
+		System.out.printf("%10s%10s%10s\n", "note", "5", "95");
+		System.out.printf("%10s%10s%10s\n", "eraser", "110", "97");
+	}
+}
