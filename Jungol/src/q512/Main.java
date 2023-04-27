b63c12ae0bad3976c4c3742bd420d1a5685b23f7
Class01 <Class01@DESKTOP-9H6B9C5>
2023-04-27 14:10:59 +0900

    512

--- Jungol/src/q512/Main.java
+++ Jungol/src/q512/Main.java
@@ -4,6 +4,7 @@ public class Main {
 	public static void main(String[] args) {
 		int weight = 49;
 		double ratio = 0.2683;
+		float fRatio = 0.2683f; 
 
 		// case 1
 		System.out.printf("%d * %f = %f\n", weight, ratio, weight * ratio);
@@ -11,5 +12,8 @@ public class Main {
 		// case 2
 		System.out.print(weight + " * ");
 		System.out.printf("%f = %f\n", ratio, (weight * ratio));
+		
+		// case 3
+		System.out.printf("%d * %f = %f\n", weight, fRatio, weight * fRatio);
 	}
 }
