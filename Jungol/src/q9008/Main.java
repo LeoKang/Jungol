6fc2dfde3b7557b9e52095c0c7cff7cb0b37312b
Class01 <Class01@DESKTOP-9H6B9C5>
2023-04-28 09:57:11 +0900

    9008

---
+++ Jungol/src/q9008/Main.java
@@ -0,0 +1,20 @@
+package q9008;
+
+public class Main {
+	public static void main(String[] args) {
+		// 2nd case
+		System.out.println(" subject  score");
+		System.out.println("===============");
+		System.out.printf("%8s    %3d\n", "korean", 90);
+		System.out.printf("%8s    %3d\n", "english", 100);
+		System.out.printf("%8s    %3d\n", "computer", 80);
+		System.out.println();
+
+		// 1st case
+		System.out.println(" subject  score");
+		System.out.println("===============");
+		System.out.println("  korean     90");
+		System.out.println(" english    100");
+		System.out.println("computer     80");
+	}
+}
