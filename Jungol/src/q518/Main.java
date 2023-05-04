e09322d450a481b28806d4ee762b45d25c830ca5
Class01 <Class01@DESKTOP-9H6B9C5>
2023-05-04 15:59:55 +0900

    518

---
+++ Jungol/src/q518/Main.java
@@ -0,0 +1,26 @@
+package q518;
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int[] num = new int[3];
+		for (int i = 0; i < num.length; i++) {
+			num[i] = sc.nextInt();
+		}
+		sc.close();
+
+//		for (int i = 0; i < num.length; i++) {
+//			System.out.print(num[i] + " ");
+//		}
+
+		int sum = 0;
+		for (int i = 0; i < num.length; i++) {
+			sum += num[i];	// 합계
+		}
+		
+		System.out.println("sum : " + sum);
+		System.out.println("avg : " + (sum / num.length));
+	}
+}
