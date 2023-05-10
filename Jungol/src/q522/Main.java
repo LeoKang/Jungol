0c565cc3dc6c7cb6e26b1d964c5f089007f4b53d
Class01 <Class01@DESKTOP-9H6B9C5>
2023-05-10 17:57:12 +0900

    522

---
+++ Jungol/src/q522/Main.java
@@ -0,0 +1,41 @@
+package q522;
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int[] inp = new int[2];
+		for (int i = 0; i < inp.length; i++) {
+			inp[i] = sc.nextInt();
+		}
+		sc.close();
+
+//		for (int i = 0; i < inp.length; i++) {
+//			System.out.print(inp[i] + " ");
+//		}
+		
+		if(inp[0] == inp[1]) {
+			System.out.println("1");
+		}else {
+			System.out.println("0");
+		}
+		if(inp[0] != inp[1]) {
+			System.out.println("1");
+		}else {
+			System.out.println("0");
+		}
+		
+		int res1 = (inp[0] == inp[1]) ? 1 : 0;
+		int res2 = (inp[0] != inp[1]) ? 1 : 0;
+		System.out.println(res1);
+		System.out.println(res2);
+	}
+}
+
+
+
+
+
+
+
