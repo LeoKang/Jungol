a6699923283f3b3a396ad13e295ed24791d38153
Class01 <Class01@DESKTOP-9H6B9C5>
2023-05-16 17:57:40 +0900

    596

---
+++ Jungol/src/q596/Main.java
@@ -0,0 +1,18 @@
+package q596;
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String str = sc.next();
+		int num = sc.nextInt();
+		sc.close();
+
+		System.out.println(str + " " + num);
+
+		for (int i = str.length() - 1; (num > 0 && i >= 0); i--, num--) {
+			System.out.print(str.charAt(i));
+		}
+	}
+}
