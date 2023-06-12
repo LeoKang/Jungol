2afb6437ca8e532e1f08bdab5680708dd1a8d6ff
Class01 <Class01@DESKTOP-9H6B9C5>
2023-06-12 13:44:32 +0900

    120

---
+++ Jungol/src/q120/Main.java
@@ -0,0 +1,24 @@
+package q120;
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n1 = sc.nextInt();
+		int n2 = sc.nextInt();
+		sc.close();
+
+//		System.out.println(n1 + " " + n2);
+
+		// 2.
+		System.out.println(Math.abs(n1 - n2));
+
+		// 1.
+//		if (n1 >= n2) {
+//			System.out.println(n1 - n2);
+//		} else {
+//			System.out.println(n2 - n1);
+//		}
+	}
+}
